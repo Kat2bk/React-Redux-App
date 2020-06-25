364e3263797fafcb406674cfca28943b89bccbf9
unknown <Katherine2bk@gmail.com>
2020-06-25 14:48:41 -0700

    set up app, added actions and reducers folder, set up component

---
+++ app/src/SunFacts.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const SunFacts = ({}) => {
+    useEffect(() => {
+
+    }, []);
+
+
+    return (
+        <div className="sun-facts">
+            <button></button>
+            <p className="info"></p>
+            <p className="info"></p>
+        </div>
+    )
+}
